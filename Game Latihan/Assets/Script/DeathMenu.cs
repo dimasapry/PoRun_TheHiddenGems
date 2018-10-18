8446e1191202891edfddfb095dcc8866fa6860b6
Dimas Apriyandi <dimasapry@gmail.com>
2018-10-18 13:46:33 +0700

    Death Menu Result Score
    Result Death Menu
    Adjust Squished Text in Death Menu Canvas

--- Game Latihan/Assets/Script/DeathMenu.cs
+++ Game Latihan/Assets/Script/DeathMenu.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class DeathMenu : MonoBehaviour {
 
+	public Text scoreResult;
+
+	public ScoreManager theScoreManager;
+
+	//private float scoreCountDeath;
+
 	public string mainMenu;
 
+
+	void Start (){
+
+
+		//theScoreManager  = gameObject.GetComponent <ScoreManager> ();
+	}
+
+
+	void Update () {
+		
+
+		scoreResult.text = ""+ Mathf.Round(theScoreManager.scoreCount);
+
+	}
 
 	public void BackToMainMenu()
 	{
